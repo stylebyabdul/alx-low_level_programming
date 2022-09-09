6ab3979f1d331c2c25751c93dd38bd8b4124f8e1
stylebyabdul <hayzedenterprise07@gmail.com>
2022-09-08 19:59:02 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+#include <puzzle.h>
+
+int main(void)
+{
+	char puzzle [30]
+	strcpy(puzz, "Programming is like building a multilingual puzzle")
+	puts(puzzle)
+	return 0;
+}
