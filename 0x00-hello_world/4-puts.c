7dfceffa1d745a0f98ff4e3ee6a4cf4ca720c0a4
stylebyabdul <hayzedenterprise07@gmail.com>
2022-09-08 20:26:13 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *main printing
- *
- * Return always 0 success
- */
+*main printing
+*
+* Return always 0 success
+*/
 int main (void)
 {
 	puts("\"programming is like a building a multilingual puzzle");
