331fb4b597b54484fbf44afe53dcfe944dc9ed94
stylebyabdul <hayzedenterprise07@gmail.com>
2022-09-11 03:52:01 -0700

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,9 +10,8 @@ int main(void)
 	int num;
 	char letter;
 
-	for (num = 0; num <= 10; num++)
-		putchar((num % 10 ) +'0');
-	
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
 	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
 
