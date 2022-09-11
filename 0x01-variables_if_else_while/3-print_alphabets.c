7805a04c0ef0889513871fa82b7cc8318955eafb
stylebyabdul <hayzedenterprise07@gmail.com>
2022-09-10 20:30:53 -0700

    putchar

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z' letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		putchar(letter);
 	}
-	for (letter = 'A'; letter <= 'Z' letter++)
+	for (letter = 'A'; letter <= 'Z'; letter++)
 	{
 		putchar(letter);
 	}
