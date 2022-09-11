cd2b2072e7d9cf84cc8d7b31698a4bd43fe9efb5
stylebyabdul <hayzedenterprise07@gmail.com>
2022-09-11 01:40:01 -0700

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,8 +9,10 @@
 int main(void)
 {
 	int num;
-
-	for ( num = 0; num < 10; num++)
+	
+	/* Print all single digit numbers of base 1o starting from 0 */
+	for (num = 0; num < 10; num++)
+		
 		putchar((num % 10) + '0');
 
 	putchar('\n');
