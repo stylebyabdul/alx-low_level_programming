3ba89520ab1d3f9b2e93d757024bbfddeb7683f8
stylebyabdul <hayzedenterprise07@gmail.com>
2022-09-11 02:06:17 -0700

    correction

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,7 +3,7 @@
 /**
  * main - Prints the alphabet in lowercase, and then in uppercase.
  *
- * Result: Always 0.
+ * Return: Always 0.
  */
 
 int main(void)
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,7 +3,7 @@
 /**
  * main - - Prints the aplhabet in lowerscase except letter q and e
  *
- * Result: Always 0
+ * Return: Always 0
  */
 int main(void)
 {
