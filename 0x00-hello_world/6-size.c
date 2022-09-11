6e40c8e69a58d8d11aa9b9c487bca2c806557274
stylebyabdul <hayzedenterprise07@gmail.com>
2022-09-11 11:10:36 -0700

    error fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,6 @@ int main(void)
 	long long int ll;
 	float f;
 	long int l;
-	
 	printf("Size of a char: %d byte(s)\n", sizeof(c));
 	printf("Size of an int: %d byte(s)\n", sizeof(i));
 	printf("Size of a long int: %d byte(s)\n", sizeof(l));
