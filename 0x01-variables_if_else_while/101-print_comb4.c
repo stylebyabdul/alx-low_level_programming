04f5d532110a4964a20b09546e3d3118165dbb0f
stylebyabdul <hayzedenterprise07@gmail.com>
2022-09-11 07:18:44 -0700

    cobination of three

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,23 +3,23 @@
 /*
  * main - Prints all the diffent combination of three digits.
  *
- * Return: Always 0.
+ * Return: 0.
  */
 int main(void)
 {
-	int digit1 ,digit2, digit3;
+	int digit1, digit2, digit3;
 
-	for (digit1 = 0; digit1 < 3; digit1++)
+	for (digit1 = 0; digit1 < 8; digit1++)
 	{
-		for (digit2 = 0; digit2 < 3; digit2++)
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
-		       	for (digit3 = 0; digit3 < 3; digit3++)
+		       	for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 			{
 				putchar((digit1 % 10) + '0');
 				putchar((digit2 % 10) + '0');
 				putchar((digit3 % 10) + '0');
 
-				if (digit1 != digit2 && digit2 != digit3 && digit3 && digit1)
+				if (digit1 == 7 && digit2 == 8 && digit == 9)
 					continue;
 
 				putchar(',');
@@ -32,4 +32,3 @@ int main(void)
 
 	return (0);
 }
-		       	       
