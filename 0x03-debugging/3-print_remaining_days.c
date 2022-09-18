0575973446447f50f3eef42c2c5a71b5ebcda15a
stylebyabdul <hayzedenterprise07@gmail.com>
2022-09-17 22:06:11 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,7 +11,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-  if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 {
 if (month >= 2 && day >= 60)
 {
@@ -22,7 +22,7 @@ printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
-if (month == 2 && day ==60)
+if (month == 2 && day == 60)
 {
 printf("invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
