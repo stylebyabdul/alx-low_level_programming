a68d83905956271c1a0a651e91f78bf91d13ecb3
stylebyabdul <hayzedenterprise07@gmail.com>
2022-09-21 15:18:59 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,21 +14,21 @@ int _strcmp(char *s1, char *s2)
 
 	while (i)
 	{
-		if (s1(i) == '\0' && s2(i) == '\0')
+		if (s1[i] == '\0' && s2[i] == '\0')
 			break;
-		else if (s1(i) == '\0')
+		else if (s1 [i] == '\0')
 		{
-			diff = s2(i);
-			break;
-		}
-		else if (s2(i) == '\0')
-		{
-			diff = s1(i);
+			diff = s2[i]
 			break;
 		}
-		else if (s1(i) != s2(1))
+		else if (s2[i] == '\0')
 		{
-			diff = s1(i) - s2(i);
+			diff = s1[i];
+			break;
+		}
+		else if (s1(i) != s2(i))
+		{
+			diff = s1[i] - s2(i);
 			break;
 		}
 		else
