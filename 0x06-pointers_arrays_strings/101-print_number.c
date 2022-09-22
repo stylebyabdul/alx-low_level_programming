9dc5a6a51800d13b23c0a95c18c5a62dfd40a498
stylebyabdul <hayzedenterprise07@gmail.com>
2022-09-22 10:38:50 -0700

    print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,12 +2,15 @@
 
 /**
  * print_number - Prints an integer.
+ * only using the putchar function.
+ * noarrays and pointers.
  * @n: The integer to be printed.
+ * Return: Void.
  */
 void print_number(int n)
 {
 	unsigned int num = n;
-
+	
 	if (n < 0)
 	{
 		_putchar('_');
