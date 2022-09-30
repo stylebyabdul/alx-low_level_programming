8ef70f9fac8564b6a813a219c5f7f3f1750ba0aa
stylebyabdul <hayzedenterprise07@gmail.com>
2022-09-30 16:30:06 -0700

    task 0

--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,5 +1,1 @@
-thout libraries what have we? We have no past and no future
-
-    create_static_lib.sh: Bash script that creates a static library called liball.a from all the .c files in the current directory.
-
-
+static libaries
--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 
 /**
- * _putchar - write the charcter c to stdout
- * @c: the character to print
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
  * Return: On success 1.
- * on error, -1 is returned, and errno is set approximately.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
