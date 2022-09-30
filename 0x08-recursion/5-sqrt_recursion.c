1294e57e680ce2ad340eb89dc329005030d20d70
stylebyabdul <hayzedenterprise07@gmail.com>
2022-09-29 20:40:32 -0700

    _sqrt_recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,10 +2,9 @@
 
 
 /**
- * find_sqrt - Finds the natural square root of an inputted number.
- * @num: The number to find the square root of.
- * @root: The root to be tested.
- *
+ * _sqrt_recursion - Finds the natural square root of an inputted number.
+ * @n: The number to find the square root of.
+ * 
  * Return: If the number has a natural square root - the square root.
  *          If the number does not have a natural square root - -1.
  */
@@ -15,3 +14,16 @@ int _sqrt_recursion(int n)
 		return (n);
 	return (_sqrt(0, n));
 }
+/**
+ * _sqrt - return the natural square root of number
+ * @n: test  number
+ * @x: squared number
+ *
+ * Return: the square root of n
+ */
+int _squrt(int n, imt x)
+{
+	if (n > X / 2)
+		retun (-1);
+	else if (n * n == x)
+		return (n);
